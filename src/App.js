ac4f360d452ee306cd469f4639e84d7b59350f64
Jim O'Brien <jimobrien@mailbox.org>
2019-07-25 16:20:13 +0100

    google map integrated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Map from "./components/map/";
 
 function App() {
+  const location = {lng:-7.142379, lat: 52.246502}
   return (
     <div className="App">
       <header className="App-header">
@@ -22,10 +23,22 @@ function App() {
 
 
       </header>
-
+      <div className="col-5" >
+        <Map
+            isMarkerShown
+            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDdUr8H8fDlsbF3Bhm-AhsBOZkP-FzqcUE&v=3.exp&libraries=geometry,drawing,places"
+            loadingElement={<div style={{ height: `100%` }} />}
+            location={location}
+            containerElement={<div style={{ height: `400px` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+        />
+      </div>
 
     </div>
   );
+
+
+
 }
 
 export default App;
